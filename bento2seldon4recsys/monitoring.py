0b6619af131af03f60b7871e6c9e142f83b62070
Bryan Lincoln <bryanufg@gmail.com>
2022-03-22 11:08:52 -0300

    feat(monitoring): add support for extra labels

--- bento2seldon4recsys/monitoring.py
+++ bento2seldon4recsys/monitoring.py
@@ -1,3 +1,5 @@
+from typing import Dict, List, Optional
+
 from bento2seldon.monitoring import FEEDBACK_ENDPOINT, Monitor
 from bento2seldon.seldon import DEPLOYMENT_ID
 from bentoml import BentoService
@@ -5,8 +7,12 @@ from prometheus_client.metrics import Histogram
 
 
 class RecommenderMonitor(Monitor):
-    def __init__(self, bento_service: BentoService) -> None:
-        super().__init__(bento_service)
+    def __init__(
+        self,
+        bento_service: BentoService,
+        extra_labels_per_metric: Optional[Dict[str, List[str]]] = {},
+    ) -> None:
+        super().__init__(bento_service, extra_labels_per_metric)
 
         self._ndcg = self._create_metric(
             Histogram,
@@ -29,18 +35,33 @@ class RecommenderMonitor(Monitor):
         )
 
     def observe_ndcg(
-        self, value: float, k: int, endpoint: str = FEEDBACK_ENDPOINT
+        self,
+        value: float,
+        k: int,
+        endpoint: str = FEEDBACK_ENDPOINT,
+        extra_labels_values: list = [],
     ) -> None:
-        self._ndcg.labels(DEPLOYMENT_ID, self.version, endpoint, k).observe(value)
+        self._ndcg.labels(
+            DEPLOYMENT_ID, self.version, endpoint, *extra_labels_values, k
+        ).observe(value)
 
     def observe_precision(
-        self, value: float, k: int, endpoint: str = FEEDBACK_ENDPOINT
+        self,
+        value: float,
+        k: int,
+        endpoint: str = FEEDBACK_ENDPOINT,
+        extra_labels_values: list = [],
     ) -> None:
-        self._precision.labels(DEPLOYMENT_ID, self.version, endpoint, k).observe(value)
+        self._precision.labels(
+            DEPLOYMENT_ID, self.version, endpoint, *extra_labels_values, k
+        ).observe(value)
 
     def observe_average_precision(
-        self, value: float, endpoint: str = FEEDBACK_ENDPOINT
+        self,
+        value: float,
+        endpoint: str = FEEDBACK_ENDPOINT,
+        extra_labels_values: list = [],
     ) -> None:
-        self._average_precision.labels(DEPLOYMENT_ID, self.version, endpoint).observe(
-            value
-        )
+        self._average_precision.labels(
+            DEPLOYMENT_ID, self.version, endpoint, *extra_labels_values
+        ).observe(value)
